a4c24a84459788e4a5ee7de7b8b96b321ad2bcc7
zactham <24833352+zactham@users.noreply.github.com>
2020-01-10 23:13:27 -0500

    Animations
    When the bird moves or dies its animation changes to its corresponding animation.

--- Flappy Bird/Assets/Scripts/Bird.cs
+++ Flappy Bird/Assets/Scripts/Bird.cs
@@ -7,6 +7,7 @@ public class Bird : MonoBehaviour
     //Checks if the player is dead or not = false means alive
     private bool isDead = false;
     private Rigidbody2D rb2d;
+    private Animator anim;
     public float upForce = 200f;
 
     // Start is called before the first frame update
@@ -14,6 +15,8 @@ public class Bird : MonoBehaviour
     {
         //When the game starts if there is a rigid body 2d it will be attached to the bird
         rb2d = GetComponent<Rigidbody2D> ();
+
+        anim = GetComponent<Animator>();
         
     }
 
@@ -28,6 +31,7 @@ public class Bird : MonoBehaviour
                 //Move the bird up, increase its y position
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger ("Flap");
             }
         }
     }
@@ -36,5 +40,6 @@ public class Bird : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         isDead = true; // so the bird cant be clicked and move upward anymore
+        anim.SetTrigger ("Die");
     }
 }
