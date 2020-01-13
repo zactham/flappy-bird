e5e6e8b98f8a4a3749d29003ca0ea0d551b5de7c
zactham <24833352+zactham@users.noreply.github.com>
2020-01-12 21:16:39 -0500

    columns spawn game is finished
    Columns spawn at random y positions each time the player makes it thru.  The game is finished

---
+++ Flappy Bird/Assets/Scripts/ColumnPool.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnPool : MonoBehaviour {
+
+    public int columnPoolSize = 5;
+    private GameObject[] columns;
+    public GameObject columnPrefab;
+    private Vector2 objectPoolPosition = new Vector2(-15, -25);
+    public float spawnRate = 4f; // how often a new col is spawned before the player
+    private float timeSinceLastSpawned;
+    public float columnMin = -1f;
+    public float columnMax = 3.5f;
+    private float spawnXPosition = 10f;
+    private int currentColumn = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        columns = new GameObject[columnPoolSize];
+        for (int i = 0; i < columnPoolSize; i++)
+        {
+            columns[i] = (GameObject)Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastSpawned += Time.deltaTime;
+        if (GameControl.instance.gameOver == false && timeSinceLastSpawned >= spawnRate)
+        {
+            timeSinceLastSpawned = 0;
+            //Spawns a col in at a random y position
+            float spawnYPosition = Random.Range(columnMin, columnMax);
+            columns[currentColumn].transform.position = new Vector2(spawnXPosition, spawnYPosition);
+            currentColumn++;
+            if (currentColumn >= columnPoolSize)
+            {
+                currentColumn = 0;
+            }
+        }
+    }
+}
